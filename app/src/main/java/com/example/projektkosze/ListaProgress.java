2f9b46bd5bc336808d061bfc2cc528335c0870f1
Hikosso <onikmarcin@wp.pl>
2022-11-27 19:13:45 +0100

    Popup

---
+++ app/src/main/java/com/example/projektkosze/ListaProgress.java
@@ -0,0 +1,14 @@
+package com.example.projektkosze;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaProgress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_progress);
+    }
+}
