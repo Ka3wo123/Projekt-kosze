b57ab36f017381b2c39783ff045c7c8906a9b96b
Hikosso <onikmarcin@wp.pl>
2022-11-27 20:53:46 +0100

    dodanie elementow do layoutu

---
+++ app/src/main/java/com/example/projektkosze/Bin.java
@@ -0,0 +1,16 @@
+package com.example.projektkosze;
+
+public class Bin {
+    public Bin(Double latitude, Double longtitude, String nazwa) {
+        this.latitude = latitude;
+        this.longtitude = longtitude;
+        this.nazwa = nazwa;
+
+    }
+
+    Double latitude;
+    Double longtitude;
+    int zapełnienie;
+    String nazwa;
+
+}
