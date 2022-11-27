046e31f77e3a99f199a710c261988e8a3d7fe21c
Kajetan Nowak <kajtek2n@gmail.com>
2022-11-27 10:12:01 +0100

    MapsActivity to MainActivity changed

--- app/src/main/java/com/example/projektkosze/MainActivity.java
+++ app/src/main/java/com/example/projektkosze/MainActivity.java
@@ -1,33 +1,17 @@
 package com.example.projektkosze;
 
-import androidx.fragment.app.FragmentActivity;
-
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.projektkosze.databinding.ActivityMapsBinding;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
-
-    private GoogleMap mMap;
-    private ActivityMapsBinding binding;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMapsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
     }
 
 }
