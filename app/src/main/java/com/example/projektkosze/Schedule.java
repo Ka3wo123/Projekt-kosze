7ccebbd8d80015366f83de54c48ecaac7c0e6eca
Kajetan Nowak <kajtek2n@gmail.com>
2022-11-28 18:16:21 +0100

    calendar/schedule

---
+++ app/src/main/java/com/example/projektkosze/Schedule.java
@@ -0,0 +1,41 @@
+package com.example.projektkosze;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
+
+public class Schedule extends AppCompatActivity {
+
+    CalendarView calendarView;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+
+        calendarView = findViewById(R.id.scheduleCalendar);
+        textView = findViewById(R.id.date);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, 2022);
+        calendar.set(Calendar.MONTH, Calendar.NOVEMBER);
+        calendar.set(Calendar.DAY_OF_MONTH, 29);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+
+                String date = day + "-" + month+1 + "-" + year;
+                textView.setText(date);
+
+            }
+        });
+    }
+}
